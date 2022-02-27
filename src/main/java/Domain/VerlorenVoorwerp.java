8153a0fbccb87bba8c227508b26dba26bf227dc5
AlexanderMertens2002 <r0842789@ucll.be>
2022-02-27 14:39:21 +0100

    versie 1.0

---
+++ src/main/java/Domain/VerlorenVoorwerp.java
@@ -0,0 +1,10 @@
+package Domain;
+
+import java.util.Date;
+
+public class VerlorenVoorwerp{
+
+    public VerlorenVoorwerp(String Beschrijving, Date Datum, String Plaats, int Telefoonnr, String Email){
+
+    }
+}
